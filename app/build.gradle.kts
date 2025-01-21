12ba3e771501027e3115f8a3078f9a02e7c84d50
bonfacemasira <bonfacemasira@gmail.com>
2025-01-21 16:37:29 +0300

    upgrade compiler sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.androidtrivia"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.androidtrivia"
