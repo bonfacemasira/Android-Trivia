4f7de243453cbb93a22214f3f042ac22c8bd71d7
bonfacemasira <bonfacemasira@gmail.com>
2025-01-22 08:51:02 +0300

    add starter code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,12 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
+    dataBinding {
+        enable = true
+    }
 }
 
 dependencies {
